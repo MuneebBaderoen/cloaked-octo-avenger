2970b4bc083a7d5afe2921cbc01cc0750abd2943
Muneeb Baderoen <mbaderoen@gmail.com>
2015-05-27 00:49:03 +0200

    threejs initialization moved to matterjs renderer. Not yet rendering engine.world.bodies, simply piggybacking the game loop

--- js/engine.js
+++ js/engine.js
@@ -15,10 +15,6 @@ var Engine = function(init, update) {
 
 _.extend(Engine.prototype, {
     initialize: function(initCallback, updateCallback) {
-        this.renderer = this.getRenderer();
-        this.currentScene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
         //Matter js world initialization
         this.physEngine = Matter.Engine.create({
             render: {
@@ -33,49 +29,14 @@ _.extend(Engine.prototype, {
 
         Matter.World.add(this.physEngine.world, [boxA, boxB, ground]);
 
-        //Threejs initialization
-        var geometryA = new THREE.BoxGeometry(1, 1, 1);
-        var materialA = new THREE.LineBasicMaterial();
-        var boxA3 = new THREE.Mesh(geometryA, materialA);
+        // boxA3.position.set(400, 200);
+        // boxB3.position.set(450, 50);
 
-        var geometryB = new THREE.BoxGeometry(1, 1);
-        var materialB = new THREE.LineBasicMaterial();
-        var boxB3 = new THREE.Mesh(geometryB, materialB);
-
-        boxA3.position.set(400, 200);
-        boxB3.position.set(450, 50);
-
-        this.currentScene.add(boxA3);
-        this.currentScene.add(boxB3);
-
-        this.camera.position.z = 5;
         Matter.Engine.run(this.physEngine);
 
         // if(initCallback)
         //     initCallback();
-
-        this.render.call(this);
-    },
-
-    update: function() {
-        // boxA3.position.set(400,200);
-        // boxB3.position.set(450,50);
-    },
-    getRenderer: function() {
-        //if renderer undefined, init it and return it, else return it
-        if(this.renderer == void 0) {
-            this.renderer = renderer = new THREE.WebGLRenderer();
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            document.body.appendChild(renderer.domElement);
-        }
-        return this.renderer;
-    },
-    render: function() {
-        requestAnimationFrame(_.bind(this.render, this));
-        this.update();
-        this.renderer.render(this.currentScene, this.camera);
     }
-
 });
 
 new Engine();
--- js/renderers/matterjsRenderer.js
+++ js/renderers/matterjsRenderer.js
@@ -5,21 +5,120 @@
 //Move all the threejs stuff here.
 
 var MatterjsRenderer = {
-	frameCount: 0,
-	create: function(){
-		return {
-			controller: MatterjsRenderer
-		}
-	},
-	world: function(engine){
-		console.time('ms');
-		console.log('inside render loop', this.frameCount++);
-		var time = console.timeEnd('ms');
-		console.log(time);
-	},
-	clear: function(renderer){
-		console.log('clearing');
-	}
+    frameCount: 0,
+    renderer: undefined,
+    rotation: {
+        x: 1,
+        y: 1
+    },
+    getRenderer: function() {
+        if(this.renderer == void 0) {
+            this.renderer = renderer = new THREE.WebGLRenderer();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            document.body.appendChild(renderer.domElement);
+        }
+        return this.renderer;
+    },
+    create: function(options) {
+        var defaults = {
+            controller: MatterjsRenderer,
+            element: null,
+            canvas: null,
+            options: {
+                width: 800,
+                height: 600,
+                pixelRatio: 1,
+                background: '#fafafa',
+                wireframeBackground: '#222',
+                hasBounds: false,
+                enabled: true,
+                wireframes: true,
+                showSleeping: true,
+                showDebug: false,
+                showBroadphase: false,
+                showBounds: false,
+                showVelocity: false,
+                showCollisions: false,
+                showSeparations: false,
+                showAxes: false,
+                showPositions: false,
+                showAngleIndicator: false,
+                showIds: false,
+                showShadows: false,
+                showVertexNumbers: false,
+                showConvexHulls: false,
+                showInternalEdges: false
+            }
+        };
+
+        var renderObject = _.extend(defaults, options);
+
+        renderObject.scene = new THREE.Scene();
+        renderObject.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+        // //render.canvas = render.renderObject || _createCanvas(render.options.width, render.options.height);
+        // //render.context = render.canvas.getContext('2d');
+        renderObject.textures = {};
+
+        // render.bounds = render.bounds || { 
+        //     min: { 
+        //         x: 0,
+        //         y: 0
+        //     }, 
+        //     max: { 
+        //         x: render.options.width,
+        //         y: render.options.height
+        //     }
+        // };
+
+        // // if (render.options.pixelRatio !== 1) {
+        // //     Render.setPixelRatio(render, render.options.pixelRatio);
+        // // }
+
+        // if (_.isElement(render.element)) {
+        //     render.element.appendChild(render.canvas);
+        // } else {
+        //     Common.log('Render.create: options.element was undefined, render.canvas was created but not appended', 'warn');
+        // }
+
+        this.getRenderer();
+        return renderObject;
+    },
+    world: function(engine) {
+        console.time('ms');
+        console.log('inside render loop', this.frameCount++);
+        var time = console.timeEnd('ms');
+        console.log(time);
+
+        var scene = this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+        this.camera.position.z = 5;
+
+        //_.each(engine.world.bodies, function(physObject) {
+        // var bb = physObject.bounds;
+
+        var geometryA = new THREE.BoxGeometry(1, 1, 1);
+        var materialA = new THREE.MeshBasicMaterial({
+            color: 0x00ff00
+        });
+        var box = new THREE.Mesh(geometryA, materialA);
+
+        scene.add(box);
+        //});
+
+        this.rotation.x += 0.05;
+        this.rotation.y += 0.05;
+
+        box.rotation.x = this.rotation.x;
+        box.rotation.y = this.rotation.y;
+
+        this.renderer.render(this.scene, this.camera);
+
+    },
+    clear: function(renderer) {
+        console.log('clearing');
+    }
 };
 
 module.exports = MatterjsRenderer;
