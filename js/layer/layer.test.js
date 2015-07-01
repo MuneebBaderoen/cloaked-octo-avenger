3181ae66b1bd99e5190cec5565199cb900c19539
Muneeb Baderoen <mbaderoen@gmail.com>
2015-07-01 13:01:31 +0200

    Added tests for layer. coverage at 100%. Will still need to add more specific tests

--- js/layer/layer.test.js
+++ js/layer/layer.test.js
@@ -9,19 +9,33 @@ describe('Tests suite for Layer.js', function () {
         callbacksExecuted;
 
     beforeEach(function () {
+        eventsInstance = new Events();
+
+        callbackCount = 0;
+        callbacksExecuted = [];
+
         layerInstance = new Layer({
             events: {
                 'listenEvent': function (source, data) {
                     callbackCount++;
                     callbacksExecuted.push('listenEvent');
                 }
+            },
+            initialize: function () {
+                callbackCount++;
+                callbacksExecuted.push('initialize');
+            },
+            update: function () {
+                callbackCount++;
+                callbacksExecuted.push('update');
+            },
+            render: function () {
+                callbackCount++;
+                callbacksExecuted.push('render');
             }
         });
 
-        eventsInstance = new Events();
 
-        callbackCount = 0;
-        callbacksExecuted = [];
     });
 
     afterEach(function () {
@@ -32,7 +46,7 @@ describe('Tests suite for Layer.js', function () {
     it('should be listening if any events are specified', function () {
         eventsInstance.trigger('listenEvent', {});
         expect(callbackCount)
-            .toBe(1);
+            .toBe(2);
     });
 
     it('should trigger callbacks for events added after init', function () {
@@ -43,9 +57,54 @@ describe('Tests suite for Layer.js', function () {
 
         eventsInstance.trigger('testEvent listenEvent', {});
 
+        //will have fired initialize
         expect(callbackCount)
-            .toBe(2);
+            .toBe(3);
     });
 
+    it('should fire initialize callback if it is specified', function () {
+        expect(callbackCount)
+            .toBe(1)
+        expect(callbacksExecuted)
+            .toContain('initialize');
+    });
+
+    it('should run normal update if callback is not specified', function () {
+        layerInstance = new Layer({});
+        layerInstance._update();
+        expect(callbackCount)
+            .toBe(1)
+        expect(callbacksExecuted)
+            .not.toContain('update');
+    });
+
+    it('should fire update callback if it is specified', function () {
+        //will have fired initialize
+        layerInstance._update();
+
+        expect(callbackCount)
+            .toBe(2)
+        expect(callbacksExecuted)
+            .toContain('update');
+    });
+
+    it('should run normal render if callback is not specified', function () {
+        layerInstance = new Layer({});
+        layerInstance._render();
+        expect(callbackCount)
+            .toBe(1)
+        expect(callbacksExecuted)
+            .not.toContain('render');
+    });
+
+    it('should fire render callback if it is specified', function () {
+        //will have fired initialize
+        layerInstance._render();
+
+        expect(callbackCount)
+            .toBe(2)
+        expect(callbacksExecuted)
+            .toContain('render');
+    });
 
 });
